5af0e8facfb4696c4ba88b2669c796681597a769
Elliot643 <elliot643@hotmail.com>
2019-10-23 09:50:46 +0200

    things

---
+++ src/components/UserCard.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="userCard">
+    <img alt="userPic" src="../assets/userpic.png">
+    <h1>Welcome to Hinder {{this.string}}</h1>
+  </div>
+</template>
+
+<script>
+    let string = "this is js stuff";
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/image1.png">
-    <h1>Welcome to Hinder</h1>
+    <h1>Welcome to Hinder {{ this.string }}</h1>
   </div>
 </template>
 
+<script>
+    let string = "this is js stuff";
+</script>
